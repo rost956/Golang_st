088ad05e8b8ab07cc267022f18eea27ab7bc4641
rost956 <rost956@mail.ru>
2025-06-15 16:05:02 +0300

    Exercise done

--- pass_fail.go
+++ pass_fail.go
@@ -2,15 +2,28 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
-	fileInfo, err := os.Stat("my.txt")
-	if err != nil {
+	fmt.Print("Enter a grade: ")
+	reader := bufio.NewReader(os.Stdin)   //создаем для чтения потока ввода
+	input, err := reader.ReadString('\n') //читаем до \n
+	if err != nil {                       //если есть ошибка
 		log.Fatal(err)
 	}
-	fmt.Println(fileInfo)
+	status := ""
+	input = strings.TrimSpace(input)            //убираем в конце \n
+	grade, err := strconv.ParseFloat(input, 64) //переводим str в float
+	if grade >= 60 {                            //условие
+		status = "passing"
+	} else {
+		status = "failing"
+	}
+	fmt.Println("A grade of", grade, "is", status) //результат
 }
