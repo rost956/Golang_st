0fbc8db14464839c3bf15917d29a0e14134cff56
rost956 <rost956@mail.ru>
2025-06-15 12:10:56 +0300

    Переменные

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
-import (
-	"fmt"
-	"reflect"
-	"strings"
-)
+import "fmt"
 
 func main() {
-	fmt.Println(reflect.TypeOf(false))
-	fmt.Println(strings.Title("headfirstgo"))
+	var quantity int
+	var length, width float64
+	var customerName string
+	quantity = 4
+	length, width = 1.2, 2.4
+	customerName = "Damon Cole"
+	fmt.Println(customerName)
+	fmt.Println("has ordered", quantity, "sheets")
+	fmt.Println("each with an area of")
+	fmt.Println(length*width, "square merers")
+
 }
