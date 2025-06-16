b77b11ab9ca4b80568522dfe3df9c63b9e762e2d
rost956 <rost956@mail.ru>
2025-06-16 17:49:25 +0300

    Added program for calculate count of painting

---
+++ paint.go
@@ -0,0 +1,23 @@
+// A program for calculate value for painting
+package main
+
+import (
+	"fmt"
+)
+
+func paintNeeded(width float64, heigth float64) (float64, error) {
+	if width < 0 {
+		return 0, fmt.Errorf("A width of %0.2f if invalid", width)
+	}
+	if heigth < 0 {
+		return 0, fmt.Errorf("A heigth of %0.2f if invalid", heigth)
+	}
+	area := width * heigth
+	return area / 10.0, nil
+}
+
+func main() {
+	amount, err := paintNeeded(4.2, -3.0)
+	fmt.Println(err)
+	fmt.Printf("%0.2f liters needed\n", amount)
+}
