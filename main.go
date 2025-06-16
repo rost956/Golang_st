58b7cbd57e7f2e1d72a68ae3eb24933de405ae65
rost956 <rost956@mail.ru>
2025-06-16 17:29:57 +0300

    Format print

--- main.go
+++ main.go
@@ -2,12 +2,18 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
 func main() {
-	var broken string = "G# r#cks!"
-	replacer := strings.NewReplacer("#", "o")
-	fixed := replacer.Replace(broken)
-	fmt.Println(fixed)
+	var str string = "Rostik"
+	fmt.Printf("About one-third: %0.2f\n", 40.0/3.0)
+	fmt.Printf("Hello, %s\n", str)
+	resultStr := fmt.Sprintf("I'm %s\n", str)
+	fmt.Print(resultStr)
+
+	fmt.Printf("%12s | %s\n", "Product", "Cost in Cents")
+	fmt.Println("------------------------------")
+	fmt.Printf("%12s | %5d\n", "Stamps", 50)
+	fmt.Printf("%12s | %5d\n", "Paper clips", 100)
+	fmt.Printf("%12s | %5d\n", "Tape", 5)
 }
